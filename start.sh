b2d46dd29a8eb0eb090257b7b2336c21928e9229
opestro <mahdiharzallah21@gmail.com>
2024-06-10 22:08:10 +0100

    update some extra information

--- start.sh
+++
@@ -1,8 +0,0 @@
-# start.sh
-#!/bin/bash
-
-# Execute pre-container startup commands
-echo "Running pre-startup commands"
-sudo chmod -R 777 addons
-sudo chmod -R 777 etc
-sudo chmod -R 777 postgresql
