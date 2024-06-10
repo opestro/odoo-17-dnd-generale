762dfd74def546ecee15b1699e2bb593cd1e2b6d
opestro <mahdiharzallah21@gmail.com>
2024-06-10 20:18:23 +0100

    update some extra information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy custom addons to the desired location
-cp -r /mnt/custom-addons/* /mnt/extra-addons/
+cp -r /addons/* /mnt/extra-addons/
 
 # Set permissions for the copied addons
 chmod -R 777 /mnt/extra-addons
